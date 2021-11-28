2c55d234c29f1fc8180a3cb8f301b30ab645b4fd
RestInPierce <68302334+RestInPierce@users.noreply.github.com>
2021-11-28 00:47:35 +0000

    Add files via upload

---
+++ level2.js
@@ -0,0 +1,1 @@
+function passcheck(){var pass=document.getElementById("pass");if(pass.value=="IHopeYouDidntLookAtSourceCode"){window.open("level3.html");return true}else{alert("incorrect");return false}}
